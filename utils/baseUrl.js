7bd83a4610758cb557c29e0251448f73aa864cc2
unknown <aleksandlario@gmail.com>
2020-04-22 13:42:50 +0100

    Fix url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://mern-stack-demo-store.now.sh"
+    ? "https://palo-alto-redux.now.sh"
     : "http://localhost:3000";
 
 export default baseUrl;
